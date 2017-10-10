0aaad5281af00acd86e177fb36535e543bf532eb
ChloeGobe <chloe.gobe@student.ecp.fr>
2017-10-10 16:03:06 +0200

    Ajout de la méthode get_trajet_traffic

--- classes_trajet.py
+++ classes_trajet.py
@@ -9,8 +9,32 @@ class Trajet:
         self.etape_iti = []
         self.temps_trajet = 0
 
-    def get_temps_pied (self):
-        """Fait appel au service web Google Maps pour les trajets a pied"""
-        web_services_google = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, "WALKING")
-        temps = web_services_google.get_time()
-        return temps
+
+    def get_trajet_pieton (self):
+        """Fait appel au service web Google Maps pour les trajets a pied
+        Retourne un json avec le temps de trajet et les etapes de l'itineraire"""
+        web_services_pied = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, "WALKING")
+        dic = {
+            'duration' : web_services_pied.get_time(),
+            "etapes" : web_services_pied.get_etapes(),
+            "mode" : "WALKING"
+        }
+        return dic
+
+
+    def get_trajet_transit(self):
+         """Fait appel au service web Google Maps pour les trajets en metro
+        Retourne un json avec le temps de trajet et les etapes de l'itineraire"""
+         web_services_metro = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, "TRANSIT")
+         temps = web_services_metro.get_time()
+         etapes = web_services_metro.get_etapes()
+         dic = {
+             'duration': web_services_metro.get_time(),
+             "etapes": web_services_metro.get_etapes(),
+             "mode" : "TRANSIT"
+         }
+         return dic
+
+
+
+

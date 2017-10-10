e60d92d286925afd518ad599e976f6223064bd91
ChloeGobe <chloe.gobe@student.ecp.fr>
2017-10-10 15:13:45 +0200

    methode google maps a pied class Trajet

---
+++ Classes Trajets.py
@@ -0,0 +1,22 @@
+##Classe qui définir les différents trajets
+import webservices
+
+class Trajet:
+    """Definit la classe trajet générale, notamment les trajets à pied"""
+
+    def __init__(self, lieu_depart, lieu_arrivee):
+        self.lieu_depart = lieu_depart
+        self.lieu_arrivee = lieu_arrivee
+        self.etape_iti = []
+        self.temps_trajet = 0
+
+
+    def temps_pied (self):
+        """Fait appel au service web Google Maps pour les trajets à pied"""
+        web_services_google = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, "WALKING")
+        temps = web_services_google.webservices.GoogleClass.get_time()
+        return temps
+
+
+
+

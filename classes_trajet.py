8d600d324058f9d274276f6b4910744520d21806
ChloeGobe <chloe.gobe@student.ecp.fr>
2017-10-20 13:12:09 +0200

    Fin du travail sur les classes
    Ecriture des classes,
    Methodes finies
    Optimisation de la taille du code

--- classes_trajet.py
+++ classes_trajet.py
@@ -4,96 +4,87 @@ import webservices
 
 
 class Trajet:
-    """Definit la classe trajet generale, notamment les trajets a pied"""
+    """Definit la classe trajet generale"""
     def __init__(self, lieu_depart, lieu_arrivee):
         self.lieu_depart = lieu_depart
         self.lieu_arrivee = lieu_arrivee
-        self.etape_iti = []
-        self.temps_trajet = 0
+        self.etape_iti = self.get_trajet_total()["etapes"]
+        self.temps_trajet = self.get_trajet_total()["duration"]
+        self.station_depart = ""
+        self.station_arrivee = ""
+        self.mode = ""
 
-
-    def get_trajet_pieton (self):
-        """Fait appel au service web Google Maps pour les trajets a pied
-        Retourne un json avec le temps de trajet et les etapes de l'itineraire"""
-        web_services_pied = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, "walking")
+    def get_trajet_specifique(self):
+        web_services = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, self.mode)
         dic = {
-            'duration' : web_services_pied.get_time(),
-            "etapes" : web_services_pied.get_etapes(),
-            "mode" : "WALKING"
+            'duration': web_services.get_time(),
+            "etapes": web_services.get_etapes(),
         }
         return dic
+
+    def get_trajet_total(self):
+        etapeA= Pieton(self.lieu_depart, self.station_depart).get_trajet_pieton()
+        etapeB = self.get_trajet_specifique(self.lieu_depart, self.lieu_arrivee, self.mode)
+        etapeC = Pieton(self.station_arrivee, self.lieu_arrivee).get_trajet_pieton()
+
+        summary = {
+            "duration": etapeA["duration"] + etapeB["duration"] + etapeC["duration"],
+            "etapes" : etapeA["etapes"] + etapeB["etapes"] + etapeC["etapse"]
+        }
+        return summary
+
+
+
+class Pieton(Trajet):
+
+    def __init__(self, lieu_depart, lieu_arrivee):
+        Trajet.__init__(self, lieu_depart, lieu_arrivee)
+        self.station_depart = self.lieu_depart
+        self.station_arrivee = self.lieu_arrivee
+        self.mode = "WALKING"
+
+
 
 
 class Metro(Trajet):
 
     def __init__(self, lieu_depart, lieu_arrivee):
         Trajet.__init__(self, lieu_depart, lieu_arrivee)
-
-    #Creer une classe Trajet Metro quand il s agira de raffiner les choix pour les trajets en metro
-    def get_trajet_transit(self):
-         """Fait appel au service web Google Maps pour les trajets en metro
-        Retourne un json avec le temps de trajet et les etapes de l'itineraire"""
-         web_services_metro = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, "transit")
-         dic = {
-             'duration': web_services_metro.get_time(),
-             "etapes": web_services_metro.get_etapes(),
-             "mode" : "TRANSIT"
-         }
-         return dic
+        self.station_depart = self.lieu_depart
+        self.station_arrivee = self.lieu_arrivee
+        self.mode = "TRANSIT"
 
 
 
-
-class Velib(Trajet):
-
+class Location(Trajet):
     def __init__(self,lieu_depart, lieu_arrivee):
         Trajet.__init__(self, lieu_depart, lieu_arrivee)
-        self.__station_depart = self.get_closest_station(lieu_depart)
-        self.__station_arrivee = self.get_closest_station(lieu_arrivee)
-        self.dataset = "stations-velib-disponibilites-en-temps-reel"
-        #Self.etape1 : self.get_traject_pieton()
-        #etape2 : self.get_traject_velo_google()
-        #etape3 : self.get_trajet_pieton()
+        self.station_depart = self.get_closest_station(lieu_depart)
+        self.station_arrivee = self.get_closest_station(lieu_arrivee)
+        self.dataset = ""
 
-    @property
-    def get_station_arrivee(self):
-        return self.__station_arrivee
-
-
-
-
-    def get_closest_station(self, adresse):
-
-        ### Transformer adresse en latitute longitude
-        ## lat, long = GoogleMaps.geloc(adresse)
-
+    def get_closest_station(self, address):
+        web_services_google = webservices.GoogleClass(self.lieu_depart, self.lieu_arrivee, "WALKING")
+        lat, lng = web_services_google.get_latlong(address)
         radius = 5000
         web_services_velib = webservices.OpendataParisClass()
         resp = web_services_velib.call_opendata(lat, lng, radius, self.dataset)
         closest_station = resp.get("records")[0].get("fields")
         return closest_station
 
-    # def get_trajet_velo()
-    # Google
 
 
-
-
-
-
-
-
-class Autolib(Trajet):
+class Velib(Location):
 
     def __init__(self, lieu_depart, lieu_arrivee):
-        Trajet.__init__(self, lieu_depart, lieu_arrivee)
+        Location.__init__(self, lieu_depart, lieu_depart)
+        self.dataset = "stations-velib-disponibilites-en-temps-reel"
+        self.mode = "BICYCLING"
+
+
+class Autolib(Location):
+
+    def __init__(self, lieu_depart, lieu_arrivee):
+        Location.__init__(self, lieu_depart, lieu_depart)
         self.dataset = "stations_et_espaces_autolib_de_la_metropole_parisienne"
-
-    def get_station_autolib(self, lat, lng):
-        radius = 5000
-        web_services_velib = webservices.OpendataParisClass()
-        resp = web_services_velib.call_opendata(lat, lng, radius, self.dataset)
-        closest_station = resp.get("records")[0].get("fields")
-        return closest_station
-
-
+        self.mode = "DRIVING"
